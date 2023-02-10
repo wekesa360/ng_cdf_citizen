3ff6243b4f7acf2b185f5c26e35e119ae4806b38
Michael Wekesa <wekesa360@outlook.com>
2023-02-10 19:57:07 +0300

    import files

--- ng_cdf_api/views.py
+++ ng_cdf_api/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
-from rest_framework
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from accounts.utils import get_authenticate_user, create_user_account
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import get_user_model, user_logged_out
