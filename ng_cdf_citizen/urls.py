073714f007f97d7a204d729d05659ed6c341ec56
Michael Wekesa <wekesa360@outlook.com>
2023-02-13 21:12:18 +0300

    add viewsets urls

--- ng_cdf_citizen/urls.py
+++ ng_cdf_citizen/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ng_cdf_api import urls as ng_cdf_api_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include('rest_framework.urls')),
+    path("ng_cdf/", include(ng_cdf_api_urls))
 ]
