9c9079ce94c67adfef618106852de566d32b6a31
Michael Wekesa <wekesa360@outlook.com>
2023-02-21 12:01:47 +0300

    add applicaiton from

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
+from django.contrib import messages
+import uuid as  unique_gen
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+def user_view(request):
+    user = User.objects.get(email=request.user.email)
