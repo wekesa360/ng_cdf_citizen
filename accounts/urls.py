93a6627ba8a4daf3453eea646b2c7875cbd0a99b
Michael Wekesa <wekesa360@outlook.com>
2023-02-28 21:18:33 +0300

    add profile urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('signin/', views.signin_view, name='signin'),
     path('signout/', views.signout_view, name='signout'),
     path('profile/<str:username>/', views.profile_view, name='profile'),
-    # path('profile/<str:username>/edit/', views.profile_edit_view, name='profile_edit'),
-    # path('profile/<str:username>/delete/', views.profile_delete_view, name='profile_delete'),
+    path('profile/<str:username>/edit/', views.edit_profile_view, name='profile_edit'),
+    path('profile/<str:username>/delete/', views.delete_profile_view, name='profile_delete'),
     path('profile/<str:username>/change-password/', views.change_password_view, name='change_password'),
     path('profile/<str:username>/change-image/', views.change_image_view, name='change_image'),
 ]
