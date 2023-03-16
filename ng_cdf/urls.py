ab75e3d06024e487759c6350a72dc8b5a58f2c29
Michael Wekesa <wekesa360@outlook.com>
2023-03-16 20:24:13 +0300

    add url endpoints

--- ng_cdf/urls.py
+++ ng_cdf/urls.py
@@ -4,10 +4,11 @@ from .views import (
     projects_view,
     # add_project_view,
     bursaries_view,
-    # add_bursary_view,
+    admin_bursary_view,
     # add_citizen_report_view,
     apply_bursary_view,
     citizen_report_view,
+    home_view,
     # add_citizen_report_view,
     delete_report_view,
     delete_bursary_view,
@@ -17,3 +18,9 @@ from .views import (
     edit_report_view
 )
 
+
+urlpatterns = [
+    path('admin/dashboard', admin_view, name='dashboard'),
+    path('', home_view, name='home'),
+    path('admin/bursary', admin_bursary_view, name='bursary'),
+]
